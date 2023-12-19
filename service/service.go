bcfab1c0e7750cf6a853fc09203d32fe77e5eab8
naveenbalaji2001 <150377130+naveenbalaji2001@users.noreply.github.com>
2023-12-19 22:00:49 +0530

    Update service.go

--- service/service.go
+++ service/service.go
@@ -1,30 +1,20 @@
-// service.go
 package service
 
 import (
 	"fmt"
+	"github.com/naveenbalaji2001/Oauth-with-go/model"
 	"github.com/zmb3/spotify"
-	
 )
 
 type SpotifyService struct {
 	SpotifyClient *spotify.Client
 }
 
-// NewSpotifyService initializes a new SpotifyService
+
 func NewSpotifyService(client *spotify.Client) *SpotifyService {
 	return &SpotifyService{SpotifyClient: client}
 }
 
-// @Summary Search for a track by ISRC code
-// @Description Search for a track by ISRC code using the Spotify API
-// @ID search-track-by-isrc
-// @Produce json
-// @Param isrc path string true "ISRC code of the track"
-// @Success 200 {object} spotify.FullTrack
-// @Failure 404 {object} map[string]interface{} "track not found"
-// @Router /search/track/{isrc} [get]
-// SearchTrackByISRC searches for a track by ISRC code using the Spotify API
 
 func (s *SpotifyService) SearchTrackByISRC(isrc string) (*spotify.FullTrack, error) {
 	query := fmt.Sprintf("isrc:%s", isrc)
@@ -33,4 +23,32 @@ func (s *SpotifyService) SearchTrackByISRC(isrc string) (*spotify.FullTrack, err
 		return nil, fmt.Errorf("track not found")
 	}
 	return &searchResult.Tracks.Tracks[0], nil
+}
+
+// @Summary Search for tracks by artist name
+// @Description Search for tracks by artist name using the Spotify API
+// @ID search-track-by-artist
+// @Produce json
+// @Param artist_name path string true "Name of the artist"
+// @Success 200 {array} model.TrackDetails
+// @Failure 404 {object} map[string]interface{} "tracks not found"
+// @Router /track/artist/{artist_name} [get]
+func (s *SpotifyService) SearchTrackByArtist(artistName string) ([]model.TrackDetails, error) {
+    query := fmt.Sprintf("artist:%s", artistName)
+    searchResult, err := s.SpotifyClient.Search(query, spotify.SearchTypeTrack)
+    if err != nil || len(searchResult.Tracks.Tracks) == 0 {
+        return nil, fmt.Errorf("tracks not found")
+    }
+    var trackDetails []model.TrackDetails
+    for _, track := range searchResult.Tracks.Tracks {
+        
+        trackDetails = append(trackDetails, model.TrackDetails{
+            ISRC:         track.ID.String(),
+            Title:        track.Name,
+            ArtistName:   track.Artists[0].Name,
+			SpotifyImage: track.Album.Images[0].URL,
+            
+        })
+    }
+    return trackDetails, nil
 }
