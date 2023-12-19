b0e00988d16bdcf151ff69e66ace25af4a82e208
naveenbalaji2001 <150377130+naveenbalaji2001@users.noreply.github.com>
2023-12-19 21:54:13 +0530

    Update controller.go

--- controller/controller.go
+++ controller/controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -30,47 +31,65 @@ func NewTrackController(dbAccessor *dao.DatabaseAccessor, spotifyService *servic
 // @Router /track/{isrc} [get]	
 func (tc *TrackController) GetTrackDetailsByISRC(c *gin.Context) {
 	isrc := c.Param("isrc")
-	existingTrack, err := tc.DatabaseAccessor.GetTrackByISRC(isrc)
 
-	if err == nil {
-		// If track is found in the database, return it
-		trackDetails := model.TrackDetails{
-			ISRC:         existingTrack.ISRC,
-			Title:        existingTrack.Title,
-			ArtistName:   existingTrack.ArtistName,
-			SpotifyImage: existingTrack.SpotifyImage,
-		}
-		c.JSON(http.StatusOK, trackDetails)
-		return
-	}
-
-	// If Track not found in database then proceed to search in Spotify
-	track, err := tc.SpotifyService.SearchTrackByISRC(isrc)
+	updatedTrack, err := tc.DatabaseAccessor.GetTrackByISRC(isrc)
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Track not found"})
+		// If the track is not found in the database, proceed to search in Spotify
+		track, err := tc.SpotifyService.SearchTrackByISRC(isrc)
+		if err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Track not found"})
+			return
+		}
+
+		// Extract track details from the Spotify search result
+		trackDetails := model.TrackDetails{
+			ISRC:         isrc,
+			Title:        track.Name,
+			ArtistName:   track.Artists[0].Name,
+			SpotifyImage: track.Album.Images[0].URL,
+		}
+
+		// Create a new Track record in the database
+		newTrack := model.Track{
+			ISRC:         isrc,
+			Title:        trackDetails.Title,
+			ArtistName:   trackDetails.ArtistName,
+			SpotifyImage: trackDetails.SpotifyImage,
+		}
+		tc.DatabaseAccessor.SaveTrack(&newTrack)
+
+		c.JSON(http.StatusOK, newTrack)
 		return
 	}
 
-	// Extract track details from the spotify search result
+	// If the track is found in the database after the update, return its details
 	trackDetails := model.TrackDetails{
-		ISRC:         isrc,
-		Title:        track.Name,
-		ArtistName:   track.Artists[0].Name,
-		SpotifyImage: track.Album.Images[0].URL,
+		ISRC:         updatedTrack.ISRC,
+		Title:        updatedTrack.Title,
+		ArtistName:   updatedTrack.ArtistName,
+		SpotifyImage: updatedTrack.SpotifyImage,
 	}
-
-	// Create a new Track record in the database
-	newTrack := model.Track{
-		ISRC:         isrc,
-		Title:        trackDetails.Title,
-		ArtistName:   trackDetails.ArtistName,
-		SpotifyImage: trackDetails.SpotifyImage,
-	}
-	tc.DatabaseAccessor.SaveTrack(&newTrack)
-
-	// Return track details as JSON
-	c.JSON(http.StatusOK, newTrack)
+	c.JSON(http.StatusOK, trackDetails)
 }
+
+// @Summary Search for tracks by artist name
+// @Description Search for tracks by artist name using the Spotify API
+// @ID search-track-by-artist
+// @Produce json
+// @Param artist_name path string true "Name of the artist"
+// @Success 200 {array} model.TrackDetails
+// @Failure 404 {object} map[string]interface{} "tracks not found"
+// @Router /track/artist/{artist_name} [get]
+func (tc *TrackController) SearchTrackByArtist(c *gin.Context) {
+	artistName := c.Param("artist_name")
+	tracks, err := tc.SpotifyService.SearchTrackByArtist(artistName)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Tracks not found"})
+		return
+	}
+	c.JSON(http.StatusOK, tracks)
+}
+
 
 // @Summary Create a new track
 // @Description Create a new track record in the database
@@ -104,4 +123,55 @@ func (tc *TrackController) CreateTrack(c *gin.Context) {
 	tc.DatabaseAccessor.SaveTrack(&newTrack)
 
 	c.JSON(http.StatusCreated, gin.H{"message": "Track record created successfully"})
+}
+
+// @Summary Update a track by ISRC
+// @Description Update an existing track record in the database by ISRC
+// @Accept json
+// @Produce json
+// @Param isrc path string true "ISRC code of the track to be updated"
+// @Param trackDetails body model.TrackDetails true "Updated track details"
+// @Success 200 {object} model.TrackDetails
+// @Failure 400 {object} map[string]string
+// @Failure 404 {object} map[string]string
+// @Router /track/{isrc} [put]
+func (ts *TrackController) UpdateTrack(isrc string, updatedTrackDetails *model.TrackDetails) (*model.TrackDetails, error) {
+	
+	existingTrack, err := ts.DatabaseAccessor.GetTrackByISRC(isrc)
+	if err != nil {
+		return nil, fmt.Errorf("track not found")
+	}
+
+	existingTrack.Title = updatedTrackDetails.Title
+	existingTrack.ArtistName = updatedTrackDetails.ArtistName
+	existingTrack.SpotifyImage = updatedTrackDetails.SpotifyImage
+
+	if err := ts.DatabaseAccessor.DB.Save(existingTrack).Error; err != nil {
+		return nil, fmt.Errorf("failed to update track in the database")
+	}
+
+	return &model.TrackDetails{
+		ISRC:         existingTrack.ISRC,
+		Title:        existingTrack.Title,
+		ArtistName:   existingTrack.ArtistName,
+		SpotifyImage: existingTrack.SpotifyImage,
+	}, nil
+}
+
+
+func (tc *TrackController) UpdateTheTrack(c *gin.Context) {
+	isrc := c.Param("isrc")
+		var updatedTrackDetails model.TrackDetails
+		if err := c.ShouldBindJSON(&updatedTrackDetails); err != nil {
+			c.JSON(400, gin.H{"error": "Invalid request body"})
+			return
+		}
+	
+		trackDetails, err := tc.UpdateTrack(isrc, &updatedTrackDetails)
+		if err != nil {
+			c.JSON(404, gin.H{"error": err.Error()})
+			return
+		}
+	
+		c.JSON(200, trackDetails)
 }
